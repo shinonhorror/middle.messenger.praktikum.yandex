4fdbf4ce3c9969d52462c5bb731e7939d2dcb5ae
shinonhorror <shinonhorror@gmail.com>
2023-02-18 14:34:03 +0300

    fix server

--- src/layouts/login/login.ts
+++ src/layouts/login/login.ts
@@ -2,7 +2,7 @@ const tpl = `<div class="container">
 <form class="login">
 <h2 class="login__title">Вход</h2>
 {{{input}}}
-<span class='error-span_reason'></span>
+<span class='error-span-reason'></span>
 {{{button}}}
 {{{link}}}
 </form>
--- src/layouts/signup/signin.ts
+++ src/layouts/signup/signin.ts
@@ -2,7 +2,7 @@ const tpl = `<div class="container">
 <form class="signin">
 <h2 class="signin__title">{{title}}</h2>
 {{{input}}}
-<span class='error-span_reason'></span>
+<span class='error-span-reason'></span>
 {{{button}}}
 {{{link}}}
 </form>
