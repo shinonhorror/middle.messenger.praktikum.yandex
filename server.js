1523998a9a875624273095e7c33d10984e21abf1
shinonhorror <shinonhorror@gmail.com>
2023-02-18 13:59:26 +0300

    fix server

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.use('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(path.join(__dirname, "dist/index.html")));
 });
 
 app.listen(PORT, () => {
